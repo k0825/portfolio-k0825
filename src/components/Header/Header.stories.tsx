d95b464543896ec693cfcf24f34d142ae058e48f
k0825 <kazukkiyo@icloud.com>
2023-02-02 14:34:02 +0000

    display flexを追加

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,10 +1,10 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
-import { Header } from './Header'
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { Header } from "./Header";
 
-export default { component: Header } as ComponentMeta<typeof Header>
+export default { component: Header } as ComponentMeta<typeof Header>;
 
 export const Default: ComponentStoryObj<typeof Header> = {
   args: {
-    title: 'デフォルト'
-  }
-}
+    title: "デフォルト",
+  },
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export const Header = () => {
-    return <header>
-        <div><Link href={"https://www.google.com"}>Google</Link></div>
-        <div><Link href={"https://www.yahoo.co.jp"}>Yahoo</Link></div>
-        <div><Link href={"https://www.nifty.com"}>nifty</Link></div>
+import styles from "./Header.module.scss";
+
+export const Header = (): JSX.Element => {
+  return (
+    <header>
+      <div className={styles.menu}>
+        <div>
+          <Link href={"https://www.google.com"}>Blog</Link>
+        </div>
+        <div>
+          <Link href={"https://www.yahoo.co.jp"}>Skill</Link>
+        </div>
+        <div>
+          <Link href={"https://www.nifty.com"}></Link>
+        </div>
+      </div>
     </header>
-}
+  );
+};
