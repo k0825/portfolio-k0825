a32b919efd0dde54c3b6ac969760fb843abc7a8f
k0825 <kazukkiyo@icloud.com>
2023-02-05 04:52:27 +0000

    ヘッダー仮完成

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,18 +5,26 @@ import styles from "./Header.module.scss";
 
 export const Header = (): JSX.Element => {
   return (
-    <header>
-      <div>
-        <Image src="/vercel.svg" alt="ロゴ" height={10} width={20} />
-        <div className={styles.menu}>
-          <div>
-            <Link href={"https://www.google.com"}>Blog</Link>
-          </div>
-          <div>
-            <Link href={"https://www.yahoo.co.jp"}>Skill</Link>
-          </div>
-          <div>
-            <Link href={"https://www.nifty.com"}>aaa</Link>
+    <header className={styles.header}>
+      <div className={styles.wide}>
+        <div className={styles.inner}>
+          <Image src="/next.svg" alt="ロゴ" height={33} width={150} />
+          <div className={styles.menu}>
+            <div className={styles.item}>
+              <Link href={"https://www.google.com"}>
+                <b>Blog</b>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Link href={"https://www.yahoo.co.jp"}>
+                <b>Skill</b>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Link href={"https://www.nifty.com"}>
+                <b>aaa</b>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
