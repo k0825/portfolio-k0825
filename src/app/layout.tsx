26b2d17cf32b2ea42550d2d6b96391362f4df037
k0825 <kazukkiyo@icloud.com>
2024-01-03 16:47:43 +0000

    VSCodeの設定を変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Header } from '@/components/header/Header'
-import { Footer } from '@/components/footer/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Header } from "@/components/header/Header";
+import { Footer } from "@/components/footer/Footer";
+import { MantineProvider } from "@mantine/core";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'k0825 portfolio site',
-  description: 'ポートフォリオサイト',
-}
+  title: "k0825 portfolio site",
+  description: "ポートフォリオサイト",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <MantineProvider>
+          <Header />
+          {children}
+          <Footer />
+        </MantineProvider>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <main>
       <h1>main</h1>
     </main>
-  )
+  );
 }
