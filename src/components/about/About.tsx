c26a50c75aac76655eae331459c7d71c74e40ea5
k0825 <kazukkiyo@icloud.com>
2024-01-05 09:11:08 +0000

    アイコンをtsx化した

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,47 +1,26 @@
 import { Avatar, Title } from "@mantine/core";
 
 import styles from "./About.module.scss";
+import { GitHub } from "../icons/GitHub";
+import { X } from "../icons/X";
+import { Zenn } from "../icons/Zenn";
 
 export const About = (): JSX.Element => {
-  const snsBaseUrl = "sns/";
-
-  type snsItem = {
-    name: string;
-    url: string;
-    image: string;
-  };
-
-  const snsList: snsItem[] = [
-    {
-      name: "X",
-      url: "https://twitter.com/ikaridev0825",
-      image: snsBaseUrl + "x.svg",
-    },
-    {
-      name: "zenn",
-      url: "https://zenn.dev/ikarin0825",
-      image: snsBaseUrl + "zenn.svg",
-    },
-    {
-      name: "github",
-      url: "https://github.com/k0825",
-      image: snsBaseUrl + "github.svg",
-    },
-  ];
-
   return (
     <section className={styles.about}>
       <div className={styles.profile}>
         <Avatar src="profile.jpeg" size={120} radius={120} mx="auto" />
         <Title order={2}>Kazukiyo Ikarigawa</Title>
         <div className={styles.logos}>
-          {snsList.map((sns) => {
-            return (
-              <a href={sns.url} key={sns.name} className={styles.sns}>
-                <Avatar src={sns.image} size={24} radius={2} mx="auto" />
-              </a>
-            );
-          })}
+          <a href="https://github.com/k0825">
+            <GitHub size={32} />
+          </a>
+          <a href="https://x.com/ikaridev0825">
+            <X size={32} />
+          </a>
+          <a href="https://zenn.dev/ikarin0825">
+            <Zenn size={32} />
+          </a>
         </div>
       </div>
     </section>
---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,6 @@
+type IconType = {
+  size?: number;
+  color?: string;
+  style?: React.CSSProperties;
+  others?: React.SVGAttributes<SVGElement>;
+};
