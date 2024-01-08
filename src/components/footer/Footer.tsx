7564f1cf83a9b7cfdf3fb636dc54b43b03247cf8
k0825 <kazukkiyo@icloud.com>
2024-01-08 05:32:51 +0000

    フッターの作成

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,15 @@
+import { Container, Text } from "@mantine/core";
+
+import styles from "./Footer.module.scss";
+
 export const Footer = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className={styles.footer}>
+      <Container size="md" mt={30}>
+        <Text c="dimmed" size="sm">
+          © 2024 Kazukiyo Ikarigawa. All rights reserved.
+        </Text>
+      </Container>
     </footer>
-  )
-}
+  );
+};
