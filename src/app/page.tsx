f8ef37e6da28f97866ca1783f64e05890310f6b2
k0825 <kazukkiyo@icloud.com>
2024-02-03 08:37:15 +0000

    notion削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,9 @@ import { Blog } from "@/components/blog/Blog";
 import { Experience } from "@/components/experience/Experience";
 import { Qualifications } from "@/components/qualifications/Qualifications";
 import { Skills } from "@/components/skills/Skills";
-import { getDatabase } from "@/data/notion";
 import { Container } from "@mantine/core";
 
 export default function Home() {
-  const databaseId = "808ffdcf244942e8a2084fcf751f91b8";
-  getDatabase(databaseId);
   return (
     <main>
       <Container size="md" mt={30}>
