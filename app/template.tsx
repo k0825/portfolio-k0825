07ef4a069a181855b612f0b5a4cde5303bb80f40
k0825 <kazukkiyo@icloud.com>
2025-02-01 08:33:18 +0000

    [fix] 不要ファイル修正

--- app/template.tsx
+++ app/template.tsx
@@ -4,9 +4,7 @@ import { Container } from "@mantine/core";
 
 import { Header } from "@/components/header/Header";
 import { HeaderSmp } from "@/components/header/HeaderSmp";
-import { NavSmp } from "@/components/navsmp/NavSmp";
 import { Footer } from "@/components/footer/Footer";
-import { useState } from "react";
 
 import styles from "./template.module.scss";
 
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     domains: ["d1lyij0osd7mgq.cloudfront.net"],
   },
