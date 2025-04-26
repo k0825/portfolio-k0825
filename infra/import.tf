7a6c9faffc90a75f1fdb3d39dd49d655a044bf23
k0825 <kazukkiyo@icloud.com>
2025-04-26 23:08:18 +0900

    [fix] 微修正

---
+++ infra/import.tf
@@ -0,0 +1,4 @@
+import {
+  id = "6c555ba4-8bf1-4ef5-9902-e0223bb52538"
+  to = module.cdn.aws_cloudfront_cache_policy.next_image_cache_policy
+}
--- infra/modules/cdn/cloudfront.tf
+++ infra/modules/cdn/cloudfront.tf
@@ -29,22 +29,13 @@ resource "aws_cloudfront_distribution" "distribution" {
     viewer_protocol_policy = "redirect-to-https"
   }
   ordered_cache_behavior {
-    path_pattern           = "/_next/image/*"
+    path_pattern           = "/_next/image"
+    cache_policy_id        = aws_cloudfront_cache_policy.next_image_cache_policy.id
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
-    compress               = true
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
     target_origin_id       = local.origin_id
     viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+    compress               = true
   }
   origin {
     domain_name              = replace(replace(aws_lambda_function_url.lambda_url.function_url, "https://", ""), "/", "")
@@ -64,12 +55,10 @@ resource "aws_cloudfront_distribution" "distribution" {
       restriction_type = "none"
     }
   }
+
   viewer_certificate {
-    acm_certificate_arn            = null
     cloudfront_default_certificate = true
-    iam_certificate_id             = null
-    minimum_protocol_version       = "TLSv1"
-    ssl_support_method             = null
+    ssl_support_method             = "sni-only"
   }
 }
 
@@ -82,4 +71,25 @@ resource "aws_cloudfront_origin_access_control" "oac" {
 
 data "aws_cloudfront_cache_policy" "cache_policy" {
   name = "Managed-CachingOptimized"
+}
+
+resource "aws_cloudfront_cache_policy" "next_image_cache_policy" {
+  comment     = "Cache policy for Next.js images"
+  default_ttl = 86400
+  max_ttl     = 31536000
+  min_ttl     = 1
+  name        = "NextImageCachePolicy"
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_brotli = true
+    enable_accept_encoding_gzip   = true
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "all"
+    }
+  }
 }
