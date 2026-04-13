57f249511c993f1af1d5c7fc3fab77d055f96f5b
k0825 <kazukkiyo@icloud.com>
2026-04-14 00:33:36 +0900

    [mod] 簡素化

--- components/about/About.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { JSX } from "react";
-import { About } from "./About";
-
-export default {
-  component: About,
-  title: "About",
-};
-
-export const Default = (): JSX.Element => <About />;
